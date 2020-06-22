def2d621970b55c52c00812ba2ebbeec531fe59e
chxpz <cryptogranacap@gmail.com>
2020-06-22 19:19:04 -0300

    included react

---
+++ psicomotor/src/Components/main-page.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+    render(){
+      return (
+        <div>
+         <h1> Website informativo para abordagem de pessoas com agitação psicomotora com comportamento
+         agressivo para profissionais de enfermagem.
+        </h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Main;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
